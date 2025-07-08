3d2c9c3a927fb0e9c6d4722f0a0ceed1ace6495c
Valentin <zaheil444@gmail.com>
2025-07-08 00:57:25 -0300

    fixes11

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,10 @@ const Header = ({ clientName, children }) => {
     return (
         <header className="header-main">
             <nav className="navbar">
-                <div className="container d-flex justify-content-between align-items-center">
-                    <span className="navbar-brand">{clientName}</span>
-                    <div>
-                        {children}
-                    </div>
-                </div>
+        <div className="container d-flex flex-column flex-md-row justify-content-center justify-content-md-between align-items-center">
+        <span className="navbar-brand mb-2 mb-md-0">{clientName}</span>
+        <div>{children}</div>
+        </div>
             </nav>
         </header>
     );
